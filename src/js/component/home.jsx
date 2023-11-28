f87e36ea090bcde888a9a4ccec285fb188b8162a
JoseArguinzones <145572262+JoseArguinzones@users.noreply.github.com>
2023-11-28 22:37:35 +0000

    listado de tareas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,23 +1,46 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const[tarea,setTarea]=useState("");
+	const[lista,setLista]=useState([]);
+		function guardartarea(e){
+			e.preventDefault()
+			setLista([...lista,tarea]);
+			setTarea("");
+		}
+
+		function eleminartarea(id){
+			let actualizado=[]
+			actualizado=lista.filter((item,index)=>{
+				if(index!=id){
+					return item;
+				}
+			})
+			setLista(actualizado);
+		}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
+		<div className="text-center container">
+			<input className="form-control" type="text" value={tarea} onChange={(e)=>setTarea(e.target.value)}/ >
+				<button className="btn btn-success" onClick={guardartarea}>
+					Agregar Tarea
+				</button>
+			<div>
+				<ul className="list-group">
+					{lista.map((item,id)=>(
+						<li className="list-group-item" key={id}>{item}
+							<button className="btn btn-danger float-end" onClick={(e)=>eleminartarea(id)}>
+								x
+							</button>
+						</li>
+					))}
+				</ul>
+			</div>
 			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+				tareas pendientes:{lista.length}
 			</p>
 		</div>
 	);
